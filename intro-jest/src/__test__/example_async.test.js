17f313c95bc413daea6ee1766f3c12f35d5998c9
CristianLegion <108194989+CristianSombra@users.noreply.github.com>
2025-01-02 16:57:52 -0300

    async, setup and mocks tests.

---
+++ intro-jest/src/__test__/example_async.test.js
@@ -0,0 +1,32 @@
+// callbacks
+
+const asyncCallBack = (cb) => {
+    setTimeout(() => {
+        cb(true);
+    }, 1000);
+};
+
+
+//promises
+
+const asyncPromise = () => new Promise((resolve) => resolve(true));
+
+
+
+describe('async code', () => {
+    test('example of async with callback', (done) => {
+        asyncCallBack((result) => {
+            expect(result).toBe(true)
+            done();
+        });
+    });
+
+    test('example of async with promises', () => {
+        return asyncPromise().then((result) => expect(result).toBe(true));
+    });
+
+    test('example of async with async await', async () => {
+    const result = await asyncPromise();
+    expect(result).toBe(true)
+    });
+});
