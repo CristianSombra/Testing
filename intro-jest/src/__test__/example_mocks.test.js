afb84fe5ad4a6e641cbc695b1b2987a565621477
CristianLegion <108194989+CristianSombra@users.noreply.github.com>
2025-01-03 01:55:26 -0300

    Update example_mocks.test.js

--- intro-jest/src/__test__/example_mocks.test.js
+++ intro-jest/src/__test__/example_mocks.test.js
@@ -1,9 +1,16 @@
 test('first example', () => {
-    const myMock = jest.fn();
+    const myMock = jest.fn()
+    .mockReturnValueOnce(true)
+    .mockReturnValueOnce('Hello world')
+    .mockReturnValueOnce(5);
 
-    myMock()
-    myMock()
-    myMock()
+    const result1 = myMock();
+    const result2 = myMock();
+    const result3 = myMock();
 
     expect(myMock).toHaveBeenCalledTimes(3);
+
+    expect(result1).toBe(true);
+    expect(result2).toBe('Hello world');
+    expect(result3).toBe(5);
 });
